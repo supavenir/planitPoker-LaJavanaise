d71c102847ad69e0dbe6fd43da6845d2c5888524
Louka-Fauvel <73880235+Louka-Fauvel@users.noreply.github.com>
2023-04-13 21:20:30 +0200

    Feat : form

--- nextjs/pages/rooms/form.js
+++ nextjs/pages/rooms/form.js
@@ -1,61 +1,89 @@
-import { Button, Form, Input, Radio } from 'antd';
-import { useState } from 'react';
-const App = () => {
-    const [form] = Form.useForm();
-    const [formLayout, setFormLayout] = useState('horizontal');
-    const onFormLayoutChange = ({ layout }) => {
-        setFormLayout(layout);
-    };
-    const formItemLayout =
-        formLayout === 'horizontal'
-            ? {
-                labelCol: {
-                    span: 4,
-                },
-                wrapperCol: {
-                    span: 14,
-                },
-            }
-            : null;
-    const buttonItemLayout =
-        formLayout === 'horizontal'
-            ? {
-                wrapperCol: {
-                    span: 14,
-                    offset: 4,
-                },
-            }
-            : null;
-    return (
-        <Form
-            {...formItemLayout}
-            layout={formLayout}
-            form={form}
-            initialValues={{
-                layout: formLayout,
-            }}
-            onValuesChange={onFormLayoutChange}
-            style={{
-                maxWidth: 600,
-            }}
-        >
-            <Form.Item label="Form Layout" name="layout">
-                <Radio.Group value={formLayout}>
-                    <Radio.Button value="horizontal">Horizontal</Radio.Button>
-                    <Radio.Button value="vertical">Vertical</Radio.Button>
-                    <Radio.Button value="inline">Inline</Radio.Button>
-                </Radio.Group>
-            </Form.Item>
-            <Form.Item label="Field A">
-                <Input placeholder="input placeholder" />
-            </Form.Item>
-            <Form.Item label="Field B">
-                <Input placeholder="input placeholder" />
-            </Form.Item>
-            <Form.Item {...buttonItemLayout}>
-                <Button type="primary">Submit</Button>
-            </Form.Item>
-        </Form>
-    );
+import {Button, Checkbox, Form, Input, Select} from 'antd';
+const onFinish = (values) => {
+    console.log('Success:', values);
 };
+const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+};
+const App = () => (
+    <Form
+        name="basic"
+        labelCol={{span: 8,}}
+        wrapperCol={{span: 16,}}
+        style={{maxWidth: 600,}}
+        initialValues={{remember: true,}}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+    >
+
+        <Form.Item label="Nom de la Salle" name="name"
+                   rules={[
+                       {
+                           required: true,
+                           message: 'Ajouter un nom de salle.',
+                       },
+                   ]}
+        >
+            <Input placeholder="Nom de la Salle"/>
+        </Form.Item>
+
+        <Form.Item label="Description" name="description">
+            <Input.TextArea placeholder="Description"/>
+        </Form.Item>
+
+        <Form.Item label="Points" name="points">
+            <Input placeholder="Points"/>
+        </Form.Item>
+
+        <Form.Item label="Suite" name="suite"
+                   rules={[
+                       {
+                           required: true,
+                           message: 'Ajouter une suite.',
+                       },
+                   ]}
+        >
+            <Select placeholder="Nom d'une suite." allowClear>
+                <Option value="male">male</Option>
+                <Option value="female">female</Option>
+                <Option value="other">other</Option>
+            </Select>
+        </Form.Item>
+
+        <Form.Item label="Équipe" name="team"
+                   rules={[
+                       {
+                           required: true,
+                           message: 'Ajouter un équipe.',
+                       },
+                   ]}
+        >
+            <Select placeholder="Nom d'une équipe." allowClear>
+                <Option value="male">male</Option>
+                <Option value="female">female</Option>
+                <Option value="other">other</Option>
+            </Select>
+        </Form.Item>
+
+        <Form.Item label="Propriétaire" name="user"
+                   rules={[
+                       {
+                           required: true,
+                           message: 'Ajouter un propriétaire.',
+                       },
+                   ]}
+        >
+            <Select placeholder="Nom du propriétaire." allowClear>
+                <Option value="male">male</Option>
+                <Option value="female">female</Option>
+                <Option value="other">other</Option>
+            </Select>
+        </Form.Item>
+
+        <Form.Item wrapperCol={{offset: 8, span: 16,}}>
+            <Button type="primary" htmlType="submit">Submit</Button>
+        </Form.Item>
+    </Form>
+);
 export default App;
