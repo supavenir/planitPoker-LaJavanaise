20ad87f9f8ade85b766f877e667c9d9369fdf124
Louka-Fauvel <73880235+Louka-Fauvel@users.noreply.github.com>
2023-05-22 12:07:55 +0200

    Feat : liste des US dans une Room

--- nextjs/pages/rooms/[id].js
+++ nextjs/pages/rooms/[id].js
@@ -1,12 +1,34 @@
-import {Divider, List} from "antd";
+import {Button, Divider, List, Space, Table} from "antd";
 import {API_URL} from "@/services/HttpService";
+import {DeleteFilled, EditFilled, EyeFilled} from "@ant-design/icons";
 
-const App = ({room}) => (
+const App = ({room, stories}) => (
     <>
-        <Divider orientation="center">Liste des Rooms</Divider>
-        <p>{room.name}</p>
+        <Divider orientation="center">{room.name}</Divider>
+        <Table columns={columns} pagination={{ position: ['none', 'bottomCenter'] }} dataSource={stories}/>
     </>
 );
+
+const columns = [
+    {
+        title: 'US',
+        dataIndex: 'name',
+        key: 'name',
+    },
+    {
+        title: 'Action',
+        dataIndex: 'id',
+        key: 'id',
+        align: 'center',
+        render: (id) => (
+            <Space size="middle">
+                <Button type="primary" shape="circle" icon={<EditFilled />} />
+                <Button type="primary" shape="circle" icon={<EyeFilled />} style={{background: "#73d13d"}} />
+                <Button type="primary" shape="circle" icon={<DeleteFilled />} danger />
+            </Space>
+        ),
+    },
+];
 
 export async function getStaticPaths() {
 
@@ -33,10 +55,13 @@ export async function getStaticProps(context) {
     const id = context.params.id;
     const resRoom = await fetch(API_URL+'rooms/'+id);
     const room = await resRoom.json();
+    const resStories = await fetch(API_URL+'story');
+    const stories = await resStories.json();
 
     return {
         props: {
-            room
+            room,
+            stories,
         },
     }
 
