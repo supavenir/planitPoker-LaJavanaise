afe9ac6324beb2c8db465acdd22a21c99a60cbe7
Louka-Fauvel <73880235+Louka-Fauvel@users.noreply.github.com>
2023-05-22 10:54:27 +0200

    Feat : RoomForm dans la liste des rooms

--- nextjs/pages/rooms/form.js
+++ nextjs/pages/rooms/form.js
@@ -1,5 +1,7 @@
 import {Button, Form, Input, Select} from 'antd';
 import RoomService from "@/pages/api/Room";
+import {useRouter} from "next/router";
+
 const onFinish = async (values) => {
     await RoomService.add(values);
 };
@@ -9,87 +11,99 @@ const onFinishFailed = (errorInfo) => {
 const handleChange = (value) => {
     console.log(`selected ${value}`);
 };
-const App = ({suites, teams, users}) => (
-    <Form
-        name="basic"
-        labelCol={{span: 8,}}
-        wrapperCol={{span: 16,}}
-        style={{maxWidth: 600,}}
-        initialValues={{remember: true,}}
-        onFinish={onFinish}
-        onFinishFailed={onFinishFailed}
-        autoComplete="off"
-    >
 
-        <Form.Item label="Nom de la Salle" name="name"
-                   rules={[
-                       {
-                           required: true,
-                           message: 'Ajouter un nom de salle.',
-                       },
-                   ]}
+const RoomForm = ({suites, teams, users}) => {
+    const router = useRouter();
+    const onFinish = async (values) => {
+        await RoomService.add(values);
+        await router.push('/rooms');
+    };
+    const onFinishFailed = (errorInfo) => {
+        console.log('Failed:', errorInfo);
+    };
+    const handleChange = (value) => {
+        console.log(`selected ${value}`);
+    };
+
+    return (
+        <Form
+            name="room"
+            labelCol={{span: 8,}}
+            wrapperCol={{span: 16,}}
+            style={{maxWidth: 600,}}
+            initialValues={{remember: true,}}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete="off"
+
         >
-            <Input placeholder="Nom de la Salle"/>
-        </Form.Item>
 
-        <Form.Item label="Description" name="description">
-            <Input.TextArea placeholder="Description"/>
-        </Form.Item>
+            <Form.Item label="Nom de la Salle" name="name"
+                       rules={[
+                           {
+                               required: true,
+                               message: 'Ajouter un nom de salle.',
+                           },
+                       ]}
+            >
+                <Input placeholder="Nom de la Salle"/>
+            </Form.Item>
 
-        <Form.Item label="Points" name="points">
-            <Input placeholder="Points"/>
-        </Form.Item>
+            <Form.Item label="Description" name="description">
+                <Input.TextArea placeholder="Description"/>
+            </Form.Item>
 
-        <Form.Item label="Suite" name="suite"
-                   rules={[
-                       {
-                           required: true,
-                           message: 'Ajouter une suite.',
-                       },
-                   ]}
-        >
-            <Select placeholder="Nom d'une suite." onChange={handleChange}>
-                {suites.map((suite) => (
-                    <Select.Option value={suite.id}>{suite.name}</Select.Option>
-                ))}
-            </Select>
-        </Form.Item>
+            <Form.Item label="Points" name="points">
+                <Input placeholder="Points"/>
+            </Form.Item>
 
-        <Form.Item label="Équipe" name="team"
-                   rules={[
-                       {
-                           required: true,
-                           message: 'Ajouter un équipe.',
-                       },
-                   ]}
-        >
-            <Select placeholder="Nom d'une équipe." onChange={handleChange}>
-                {teams.map((team) => (
-                    <Select.Option value={team.id}>{team.name}</Select.Option>
-                ))}
-            </Select>
-        </Form.Item>
+            <Form.Item label="Suite" name="suite"
+                       rules={[
+                           {
+                               required: true,
+                               message: 'Ajouter une suite.',
+                           },
+                       ]}
+            >
+                <Select placeholder="Nom d'une suite." onChange={handleChange}>
+                    {suites.map((suite) => (
+                        <Select.Option value={suite.id}>{suite.name}</Select.Option>
+                    ))}
+                </Select>
+            </Form.Item>
 
-        <Form.Item label="Propriétaire" name="user"
-                   rules={[
-                       {
-                           required: true,
-                           message: 'Ajouter un propriétaire.',
-                       },
-                   ]}
-        >
-            <Select placeholder="Nom du propriétaire." onChange={handleChange}>
-                {users.map((user) => (
-                    <Select.Option value={user.id}>{user.username}</Select.Option>
-                ))}
-            </Select>
-        </Form.Item>
+            <Form.Item label="Équipe" name="team"
+                       rules={[
+                           {
+                               required: true,
+                               message: 'Ajouter un équipe.',
+                           },
+                       ]}
+            >
+                <Select placeholder="Nom d'une équipe." onChange={handleChange}>
+                    {teams.map((team) => (
+                        <Select.Option value={team.id}>{team.name}</Select.Option>
+                    ))}
+                </Select>
+            </Form.Item>
 
-        <Form.Item wrapperCol={{offset: 8, span: 16,}}>
-            <Button type="primary" htmlType="submit">Submit</Button>
-        </Form.Item>
-    </Form>
-);
+            <Form.Item label="Propriétaire" name="user"
+                       rules={[
+                           {
+                               required: true,
+                               message: 'Ajouter un propriétaire.',
+                           },
+                       ]}
+            >
+                <Select placeholder="Nom du propriétaire." onChange={handleChange}>
+                    {users.map((user) => (
+                        <Select.Option value={user.id}>{user.username}</Select.Option>
+                    ))}
+                </Select>
+            </Form.Item>
+        </Form>
+    );
+}
 
 export async function getStaticProps(context) {
 
@@ -110,4 +124,4 @@ export async function getStaticProps(context) {
     }
 }
 
-export default App;
+export default RoomForm;
--- nextjs/pages/rooms/index.js
+++ nextjs/pages/rooms/index.js
@@ -2,35 +2,41 @@ import {Button, Divider, List, Modal, Space, Table, Tag} from 'antd';
 import {DeleteFilled, EditFilled, EyeFilled} from "@ant-design/icons";
 import RoomService from "@/pages/api/Room";
 import {useState} from "react";
+import RoomForm from "@/pages/rooms/form";
 
 async function handleChange(value) {
     console.log(`selected ${value}`);
     await RoomService.delete(value);
 }
 
-const [isModalOpen, setIsModalOpen] = useState(false);
-const showModal = () => {
-    setIsModalOpen(true);
-};
-const handleOk = () => {
-    setIsModalOpen(false);
-};
-const handleCancel = () => {
-    setIsModalOpen(false);
-};
+const App = ({rooms, suites, teams, users}) => {
 
-const App = ({rooms}) => (
-    <>
-        <Divider orientation="left">Liste des Rooms</Divider>
-        <Button type="primary" onClick={showModal}>Ajouter une Room</Button>
-        <Table columns={columns} pagination={{ position: ['none', 'bottomCenter'] }} dataSource={rooms}/>
-        <Modal title="Basic Modal" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
-            <p>Some contents...</p>
-            <p>Some contents...</p>
-            <p>Some contents...</p>
-        </Modal>
-    </>
-);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const showModal = () => {
+        setIsModalOpen(true);
+    };
+    const handleOk = () => {
+        setIsModalOpen(false);
+    };
+    const handleCancel = () => {
+        setIsModalOpen(false);
+    };
+
+    return (
+        <>
+            <Divider orientation="left">Liste des Rooms</Divider>
+            <Button type="primary" onClick={showModal}>Ajouter une Room</Button>
+            <Table columns={columns} pagination={{ position: ['none', 'bottomCenter'] }} dataSource={rooms}/>
+            <Modal title="Ajouter une Room" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}
+                   footer={
+                        [<Button form="room" type="primary" htmlType="submit" onClick={handleOk}>Submit</Button>]
+                    }
+            >
+                <RoomForm suites={suites} teams={teams} users={users}/>
+            </Modal>
+        </>
+    );
+}
 
 const columns = [
     {
@@ -59,10 +65,19 @@ export async function getStaticProps(context) {
     const API_URL = 'http://127.0.0.1:8090/api/';
     const resRooms = await fetch(API_URL+'room');
     const rooms = await resRooms.json();
+    const resSuites = await fetch(API_URL+'suites');
+    const suites = await resSuites.json();
+    const resTeams = await fetch(API_URL+'teams');
+    const teams = await resTeams.json();
+    const resUsers = await fetch(API_URL+'users');
+    const users = await resUsers.json();
 
     return {
         props: {
-            rooms
+            rooms,
+            suites,
+            teams,
+            users,
         },
     }
 }
